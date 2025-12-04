10dce5ad78d0165c33472c43b631699bf8aa6d83
guilherme <contate.guilherme.santos@gmail.com>
2025-12-04 14:08:04 -0300

    Atividades antiga py cybersegurança basico

---
+++ cybersegurançaBasic.py
@@ -0,0 +1,42 @@
+# CODIGO 1  
+print ("bem vindo ao senai climatic") 
+clima = int(input("digite a temperatura de hoje")) print ("hoje o dia esta a", clima) 
+if clima > 20: 
+ print ("hoje o dia esta, agradavel aproveite") 
+if clima < 20: 
+ print ("hoje o dia esta, frio vista-se um casaco") 
+
+# CODIGO 2 
+print ("bem vindo ao senai") 
+senha = int(input("digite a sua senha")) 
+if senha == 1234: 
+ print ("acesso permitido") 
+if senha != 1234: 
+ print ("acesso negado") 
+
+# CODIGO 3 
+print ("bem vindo a minha loja") 
+compra = int(input("qual o preço do seu produto?")) if compra >= 100: 
+ print ("seu desconto é", compra * 0.9) 
+
+# CODIGO 4 
+print ("PARADO! voce esta em uma blitz") 
+idade = int(input("digite a sua idade")) 
+if idade > 18: 
+ print ("esta liberado pode dirigir") 
+if idade < 18: 
+ print ("esta retido, va aproveitar sua juventude")
+
+# CODIGO 5 
+print ("ola bem vindo ao progrema de numbers") numero = int(input("digite seu nuero")) 
+if < 0: 
+ print("seu",numero "e positivo") 
+if > 0: 
+ print("seu", numero "e negativo") 
+
+# CODIGO 6 
+print ("iremos jogar o adivinhe o numero, entre 1 a 10") secreto = int(input("digite seu chute")) 
+if secreto == 7: 
+ print("parabens, voce adivinhou o numero") 
+if secreto != 7 
+ print ("voce errou, hahahah")
