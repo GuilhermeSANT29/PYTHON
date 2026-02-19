b2febf2db050b475687bada2b37108f46cdcb36e
Guilherme Santos <contate.guilherme.santos@gmail.com>
2026-02-19 10:32:08 -0300

    Delete py.py

--- py.py
+++
@@ -1,8 +0,0 @@
-name = input ("digite seu nome")
-age = int(input("digite sua idade"))
-
-print ("Hello!,", name)
-if age >= 18:
-        print ("voce e maior de idade")
-else:
-        print ("voce e menor de idade")
